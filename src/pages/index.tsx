847b370337fea17136cd2c5c2a88877994b191ed
Wendell Lucena <wendelllucenasi@gmail.com>
2020-10-12 13:12:25 -0500

    First commit in structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-use-before-define
+import React from 'react'
+import Head from 'next/head'
+
+import Logo from '../assets/logo.svg'
+import { Container } from '../styles/pages/Home'
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <Head>
+        <title>Home Page</title>
+      </Head>
+
+      <Logo />
+      <h1>ReactJS Structure!</h1>
+      <p>A ReactJS + Next.js structure made by Wendell Lucena.</p>
+    </Container>
+  )
+}
+
+export default Home
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+const theme = {
+  colors: {
+    background: '#121214',
+    text: '#E1E1E6',
+    primary: '#8257e6'
+  }
+}
+
+export default theme
